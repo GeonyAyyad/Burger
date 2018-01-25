d73501202be595fac7efe9bc5727d2b25487d4e8
Geony Ayyad <geonyayyad@Geonys-MBP.fios-router.home>
2018-01-24 22:54:52 -0500

    new changes

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burgerName, devoured)
+VALUES ("Southern", true);
+
+INSERT INTO burgers (burgerName, devoured)
+VALUES ("Barbeque Cheddar", true);
+
+INSERT INTO burgers (burgerName, devoured)
+VALUES ("Jalapeno", true);
